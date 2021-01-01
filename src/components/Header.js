0b66c27f553fbcb0b726f602ff2e85ee9d34b44e
Kieran Venison <venison@hey.com>
2021-01-01 19:14:44 +0000

    tested image in static query

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+
+const HeaderStyles = styled.div`
+  .gatsby-image-wrapper {
+    width: 125px;
+    height: 125px;
+  }
+`;
 
 export default function Header() {
-  return <header>Header</header>;
+  const { logo } = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "kieran-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 125, maxHeight: 125) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <HeaderStyles>
+      <h1>Header</h1>
+      <Img fluid={logo.childImageSharp.fluid} />
+    </HeaderStyles>
+  );
 }
