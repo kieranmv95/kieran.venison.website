99271ec98f96abc0e52e350c247cd123b6d8ce83
Kieran Venison <venison@hey.com>
2021-01-09 20:19:48 +0000

    fixed casing

---
+++ src/components/SEO.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
+export default function SEO({ title, description, children }) {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
+  return (
+    <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
+      <html lang="en" />
+      <title>{title}</title>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content={description || site.siteMetadata.description}
+      />
+      {children}
+    </Helmet>
+  );
+}
