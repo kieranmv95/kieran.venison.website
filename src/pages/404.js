41a4de4cce2662be99f7c6defe1c128b103fa1ca
Kieran Venison <venison@hey.com>
2021-01-27 17:21:04 +0000

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import SEO from '../components/SEO';
 
@@ -5,7 +6,11 @@ export default function NotFound() {
   return (
     <>
       <SEO title="Not Found" description="Did you get lost?" />
-      Not Found
+      <div className="center">
+        <h1>Are you lost?</h1>
+        <p>This page doesn't appear to exist yet!</p>
+        <Link to="/">Go back to Home</Link>
+      </div>
     </>
   );
 }
